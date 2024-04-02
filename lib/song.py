79655484a488ef5d77f2ad9b3b6b866137f4860a
Timothy Ryon <tim@timryon.com>
2024-04-02 15:17:04 -0400

    refactor code with Nancy pass all tests

--- lib/song.py
+++ lib/song.py
@@ -6,41 +6,48 @@ class Song:
     artist_count = dict()
 
     def __init__(self, name, artist, genre):
-        self.add_song_to_count(name, artist, genre)
+        type(self).add_song_to_count()
+        # self.add_song_to_count(name, artist, genre)
         self.name = name
         self.artist = artist
         self.genre = genre
-        self.add_to_genres(genre)
-        self.add_to_genre_count(genre)
-        self.add_to_artists(artist)
-        self.add_to_artist_count(artist)
-
+        type(self).add_to_genres(genre)
+        # type(self).add_to_genre_count(genre)
+        type(self).add_to_artists(artist)
+        # Song.add_to_artist_count(artist)
+# line 16 is better, but 17 is hard -coding
 
     @classmethod
-    def add_song_to_count(cls, name, artist, genre, increment=1):
-        cls.count += increment
-        cls.name = name
-        cls.artist = artist
-        cls.genre = genre
+    def add_song_to_count(cls):
+        cls.count += 1
 
+
+    # @classmethod
+    # def add_song_to_count(cls, name, artist, genre, increment=1):
+    #     cls.count += increment
+    #     cls.name = name
+    #     cls.artist = artist
+    #     cls.genre = genre
 
     @classmethod
     def add_to_genres(cls, genre):
+        cls.genre_count[genre] = cls.genre_count.get(genre,0) + 1
+        # line 34 added from own cls method that was redundant on line 49
         if genre not in cls.genres:
             cls.genres.append(genre)
-        else:
-            return
 
     @classmethod
     def add_to_artists(cls, artist):
+        cls.artist_count[artist] = cls.artist_count.get(artist ,0) + 1
+        # line 41 added from own cls method that was redundant on line 66
         if artist not in cls.artists:
             cls.artists.append(artist)
-        else:
-            return
+        # else:  not needed
+        #     return not needed; I don't need to return nothing; unnecessary...
         
-    @classmethod
-    def add_to_genre_count(cls, genre):
-        cls.genre_count[genre] = cls.genre_count.get(genre,0) + 1
+    # @classmethod
+    # def add_to_genre_count(cls, genre):
+    #     cls.genre_count[genre] = cls.genre_count.get(genre,0) + 1
 
         # my more verbose code (with TC help to get it working, but does pass tests)
         # if cls.genre_count == {}:
@@ -54,9 +61,9 @@ class Song:
         #     cls.genre_count[genre] = 1
         # print(cls.genre_count["Rap"])
 
-    @classmethod
-    def add_to_artist_count(cls, artist):
-        cls.artist_count[artist] = cls.artist_count.get(artist ,0) + 1
+    # @classmethod
+    # def add_to_artist_count(cls, artist):
+    #     cls.artist_count[artist] = cls.artist_count.get(artist ,0) + 1
 
 # my more verbose code; logic copied from above class method.  1 line above = Pythonic
         # if cls.artist_count == {}:
